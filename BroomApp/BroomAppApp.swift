e069ca96c06a52dc9178d68eff115d871df97557
canersaka <blackholeisbh@gmail.com>
2024-01-11 23:04:56 +0000

    Initial Commit

---
+++ BroomApp/BroomAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  BroomAppApp.swift
+//  BroomApp
+//
+//  Created by Caner Saka on 1/11/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BroomAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
